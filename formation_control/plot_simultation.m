cfad6cc4effa281a4b51f94139462c6ce0d0f269
Giuseppe Sensolini Arrà <46244893+giusenso@users.noreply.github.com>
2021-07-22 13:48:31 +0200

    Add files via upload

---
+++ formation_control/plot_simultation.m
@@ -0,0 +1,97 @@
+%% plot data
+
+B = out.B.Data;
+N = params.N;   % number of robots
+E = params.E;   % number of edges
+n = size(out.state.Data,2)/(N+E);	% operational space
+
+%% set colors
+leader_c = '#EE1111';
+follower_c = '#2222FF';
+faulted_c = '#9999FF';
+trajectory_c = '#006600';
+color = cell(1,N);
+color{1} = leader_c;
+for i = 2:N
+    color{i} = follower_c;
+end
+
+%% extract robots data
+robot = cell(N,1);
+for k = 1:N
+    robot{k}.x = (out.q.Data(:,n*(k-1)+1)).';
+    robot{k}.y = (out.q.Data(:,n*(k-1)+2)).';
+    robot{k}.z = (out.q.Data(:,n*(k-1)+3)).';
+end
+
+%% Initialize video
+set(gcf,'units','points','position',[400,10,600,600])
+myVideo = VideoWriter('simulations/simulation','MPEG-4'); % open video file
+myVideo.FrameRate = 10;  % can adjust this, 5 - 10 works well for me
+myVideo.Quality = 100;
+open(myVideo)
+
+fault_occurred = 0;
+s = size(out.q.Data,1);
+
+%% check steady state time instant
+for i = 1:s
+    exit_flag = 0;
+    for k = 1:N
+        if( i>s*0.9 && norm([robot{k}.x(i)-robot{k}.x(i-2); robot{k}.y(i)-robot{k}.y(i-2); robot{k}.z(i)-robot{k}.z(i-2)]) < 0.001 )
+            exit_flag = exit_flag + 1;  % robot is no moving anymore
+        end
+    end
+    if(exit_flag==N)
+        s = i;
+        break;
+    end
+end
+
+for i = 1:s
+    %% Draw edges
+    for edge = 1:E
+        if(size(find(B(:,edge,i)==1),1) ~= 0)
+            f = find(B(:,edge,i)==-1);
+            t = find(B(:,edge,i)==1);
+            line([robot{f}.x(i),robot{t}.x(i)], [robot{f}.y(i),robot{t}.y(i)], [robot{f}.z(i),robot{t}.z(i)], 'Color','#111111');
+        end
+        hold on;
+    end
+    
+    %% Draw trajectory
+    plot3(out.y1.Data(:,1),out.y1.Data(:,2),out.y1.Data(:,3), 'Color',trajectory_c,'linewidth',1.2);
+    plot3(out.y1.Data(i,1),out.y1.Data(i,2),out.y1.Data(i,3), '.','Color',trajectory_c,'MarkerSize',25);
+    
+    %% Draw robots
+    plot3(robot{1}.x(1:i), robot{1}.y(1:i), robot{1}.z(1:i), 'Color', color{1},'linewidth',1.2);
+    for k = 1:N
+        if(sum(abs(B(k,:,i)))==0 && ~fault_occurred)
+            fault_occurred = 1;
+            color{k} = faulted_c;
+        end
+        plot3(robot{k}.x(i), robot{k}.y(i), robot{k}.z(i),'.','Color',color{k},'MarkerSize',35);
+        hold on;
+        
+        % check if robot at steady state
+        if ( i>100 && norm([robot{k}.x(i)-robot{k}.x(i-2); robot{k}.y(i)-robot{k}.y(i-2); robot{k}.z(i)-robot{k}.z(i-2)]) < 0.1 )
+            exit_flag = exit_flag + 1;  % robot is not moving anymore
+        end
+    end
+    
+    grid on, view(-10,40);
+    set(gca,'XLim',[-160 80],'YLim',[-60 180],'ZLim',[0,80]);
+    pause(0.1)
+    frame = getframe(gcf); %get frame
+    writeVideo(myVideo, frame);
+    clf;
+    
+end
+
+close(myVideo)
+close all
+clear myVideo frame
+
+
+
+%
--- formation_control/simulations/sim4/init_sim4.m
+++ formation_control/simulations/sim4/init_sim4.m
@@ -1,16 +1,13 @@
 clear all
 
 %% params
-T = 140; % running time 
-params.tracking = false; %  ENABLE/DISABLE TRAJECTORY TRACKING
+T = 60; % running time 
 
-%% Robots
 n = 3;  % task space
 params.N    = 4;                % number of agents
 params.m    = [10;10;10;10];    % masses
 params.d    = [6;6;6;6];        % frict. coeff
 
-%% Edges
 params.Bfull= [ -1 0 0 +1 -1 0; 
                 1 -1 0 0 0 -1;
                 0 1 -1 0 1 0;
@@ -21,11 +18,9 @@ params.B = [   -1 0 0 +1 0 0;
                 0 0 1 -1 0 0];      % initial incidence matrix
 
 params.E    = size(params.Bfull,2);	% number of edges
-params.dc	= [10;10;10;10;10;10];	% damping coeff
-params.kc	= [8;8;8;8;8;8];	% spring constant
+params.dc	= [4;4;4;4;4;4];	% damping coeff
+params.kc	= [10;10;10;10;10;10];	% spring constant
 d = 20;
-
-%% Desired edges length
 params.z_des = [[d;0;0];
                 [0;-d;0];
                 [-d;0;0];
@@ -33,14 +28,13 @@ params.z_des = [[d;0;0];
                 [d;-d;0];
                 [-d;-d;0]];         % desired edges length
 
-%% Tanks
 params.tank_size = 100;
 params.tank_thresh = 0.25;   
 params.tank_max = params.tank_size*(1+params.tank_thresh);
 params.tank_min = params.tank_size*(1-params.tank_thresh);
 params.fault_occurred = 0;
-params.joined_edge = params.E+1;            % edge joined after fault
-params.gamma = 10^(-4)*ones(params.E,1);	% rate of exchange of energy
+params.joined_edge = params.E+1;  	% edge joined after fault
+params.gamma = 10^(-4)*ones(6,1);   % rate of exchange of energy
 
 params.gain = zeros(n*params.N,1);
 for i = 1:params.N
@@ -48,7 +42,7 @@ for i = 1:params.N
 end
 clear i
 
-%% initialize the params bus
+% initialize the params bus
 params_bus_info = Simulink.Bus.createObject(params);
 params_bus = evalin('base',params_bus_info.busName);
 
